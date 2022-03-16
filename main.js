766aed0da1874a4935a09ffb3ffba517ba800e05
Denise Troglio <overbeck.denise@gmail.com>
2022-03-16 12:35:46 +0100

    add new fetch with url

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 "use strict";
 
-const titlePage = document.querySelector(".js_title");
-
-titlePage.innerHTML = "Adopt a pretty dog and be happy toghether!";
 
 
 //AP DogsI:
 
-const getFromAPIDogs = fetch("../dataDogs/APIdogs.json")
-.then(response => response.json())
-.then(data => {
+const getFromAPIDogs = fetch("https://api.thedogapi.com/v1/breeds")
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
-});
+  });
