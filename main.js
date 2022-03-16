14bacd3780b93046c2d9dc9d2c5a821cf34f14f9
Denise Troglio <overbeck.denise@gmail.com>
2022-03-16 11:36:30 +0100

    add fetch from API

--- main.js
+++ main.js
@@ -3,3 +3,12 @@
 const titlePage = document.querySelector(".js_title");
 
 titlePage.innerHTML = "Adopt a pretty dog and be happy toghether!";
+
+
+//AP DogsI:
+
+const getFromAPIDogs = fetch("../dataDogs/APIdogs.json")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+});
