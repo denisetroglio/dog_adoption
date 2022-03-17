1376465fd84635ccff2b65d786305aa4caa29518
Denise Troglio <overbeck.denise@gmail.com>
2022-03-17 09:30:13 +0100

    add desplegable menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
-
+const menuList = document.querySelector(".js_menu");
+const menuIcon = document.querySelector(".js_menuicon");
 
 //AP DogsI:
 
@@ -9,3 +10,38 @@ const getFromAPIDogs = fetch("https://api.thedogapi.com/v1/breeds")
   .then((data) => {
     console.log(data);
   });
+
+//MENU:
+
+//muestra el menu:
+function showMenu(){
+  menuList.classList.remove('hidden');
+  console.log("muestro el menu");
+}
+
+//oculta el menu:
+function hiddenMenu(){
+  menuList.classList.add('hidden');
+  console.log("oculto el menu");
+}
+
+//muestra o oculta el menu:
+function menuDown(){
+  if(menuList.classList.contains('hidden')){
+    showMenu();
+  }
+  else{
+    hiddenMenu();
+  }
+}
+
+// evento click:
+function handleClickMenu(event){
+  event.preventDefault();
+  menuDown();
+}
+
+//evento menu:
+menuIcon.addEventListener("click", handleClickMenu);
+
+//MAP:
